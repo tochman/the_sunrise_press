2b74ebc333910c3277fa8c60292a61dc70f88f6b
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-10 15:13:33 +0100

    Fixes syntac errors in application rb

--- config/application.rb
+++ config/application.rb
@@ -14,14 +14,15 @@ Bundler.require(*Rails.groups)
 
 module TheSunrisePress
   class Application < Rails::Application
-    config.load_defaults 5.1
+    config.load_defaults 5.2
     config.generators do |generate|
-    generate.helper false
-    generate.assets false
-    generate.view_specs false
-    generate.helper_specs false
-    generate.routing_specs false
-    generate.controller_specs false
-    generate.system_tests fals
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+      generate.system_tests false
+    end
   end
 end
