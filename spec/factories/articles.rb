125d2c22f323290a3531d5a35b54d576affc49c9
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-10 20:12:56 +0100

    Changes seed file, adds validations for data columns in model adds unit tests for data presence, validation and factorybot

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :article do
-    
+    title { 'An article' }
+    description { 'It is not long' }
+    content { 'As I told you, that is it' }
+    journalist { 'Jon' }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :title }
+    it { is_expected.to have_db_column :description }
+    it { is_expected.to have_db_column :content }
+    it { is_expected.to have_db_column :journalist }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :description } 
+    it { is_expected.to validate_presence_of :content } 
+    it { is_expected.to validate_presence_of :journalist } 
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:article)).to be_valid
+    end
+  end
+
 end
