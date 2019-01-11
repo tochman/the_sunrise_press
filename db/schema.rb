6d431c9519d389467cbb7158214a1d562ab3d0df
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-11 13:03:25 +0100

    Fixes error by adding devise_for :users to routes.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_01_11_091312) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "hstore"
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
