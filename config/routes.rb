c56c40d3d5e926079a73ea72bc6e67dc8f4a4bcc
johannaanden <johannaanden@hotmail.com>
2019-01-11 12:48:45 +0100

    Removes schaffold get request

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'categories/show'
   root controller: :articles, action: :index
   resources :articles, only: [:index, :show]
   resources :categories, only: [:show]
