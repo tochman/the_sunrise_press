c6d3544cdb180f072d4d45176cc6dc886649d239
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-11 11:30:38 +0100

    Adds category navbar, adds show route for category, adds navbar to the application layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ Category.create(name: 'Weather')
 Category.create(name: 'Sport')
 
 Article.create(title: 'The storm is comming', description: 'It is going to rain tomorrow', content: 'It is going to rain alot tomorrow', journalist: 'Johanna', category_id: 1)
-Article.create(title: 'No it is not comming !', description: 'It turned out it did not rain at all!', content: 'Shocking really', journalist: 'Greg', category_id: 2)
-Article.create(title: 'Actually the sun was comming', description: 'Much better right?', content: 'It was quite warm!', journalist: 'John', category_id: 2)
+Article.create(title: 'No it is not comming !', description: 'It turned out it did not rain at all!', content: 'Shocking really', journalist: 'Greg', category_id: 1)
+Article.create(title: 'Arsenal has lost against the Fradi!', description: 'I cannot believe it either!', content: 'The world is amazed by this!', journalist: 'John', category_id: 2)
