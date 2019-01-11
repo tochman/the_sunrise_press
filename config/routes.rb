f75efb3f74f08b61d503f312f6bc94d9a276987a
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-11 11:39:55 +0100

    Adds show action for Categories controller, uses show view to iterate through all the articles in one category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'categories/show'
   root controller: :articles, action: :index
   resources :articles, only: [:index, :show]
   resources :categories, only: [:show]
