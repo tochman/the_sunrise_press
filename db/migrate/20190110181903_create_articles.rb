4b2c70692fbc458932202c968c4b440712aa4241
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-11 07:21:05 +0100

    Changes the content datatype to text

--- db/migrate/20190110181903_create_articles.rb
+++ db/migrate/20190110181903_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
 
       t.string :title
       t.string :description
-      t.string :content
+      t.text :content
       t.string :journalist
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_01_10_181903) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.string "content"
+    t.text "content"
     t.string "journalist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
