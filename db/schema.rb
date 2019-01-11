14983413642a7b0ff65d580d90a45c5f34dfee5a
Jonathan <jon1969edwards@gmail.com>
2019-01-11 09:53:11 +0100

    Creates user controller and adds step definitions for registrations test

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2019_01_10_190102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.text "content"
+    t.string "journalist"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,13 +34,6 @@ ActiveRecord::Schema.define(version: 2019_01_10_190102) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.text "content"
-    t.string "journalist"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -7,3 +7,22 @@ end
 Given("I visit the landing page") do
     visit root_path
 end
+
+Given("the following user exists") do |table|
+    table.hashes.each do |user|
+        FactoryBot.create(:user, user)
+    end 
+end
+  
+Given("I visit the page") do
+    visit root_path
+end
+  
+Given("I click {string}") do |string|
+    click_on string
+end
+  
+Given("I fill in {string} field with {string}") do |field, input|
+    fill_in field, with: input
+end
+  
