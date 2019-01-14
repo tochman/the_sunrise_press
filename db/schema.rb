6520ca77a0b195544b52aca0a69440b3946378c1
Jonathan <jon1969edwards@gmail.com>
2019-01-14 13:46:12 +0100

    Adds margins to buttons

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2019_01_14_093813) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "hstore"
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
@@ -41,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_01_14_093813) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
