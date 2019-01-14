d24137e41f76240678b1e8c97769801fae0ad0b1
johannaanden <johannaanden@hotmail.com>
2019-01-14 11:12:22 +0100

    Adds namespace to journalist and separate new and create action to the namespace

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,33 +1,11 @@
 class ArticlesController < ApplicationController
+    
     def index
         @articles = Article.all
     end
 
     def show
         @article = Article.find(params[:id])
-    end
-
-    def new
-        @article = Article.new
-        @categories = Category.all
-    end
-
-    def create
-        @article = Article.create(article_params.merge(category: find_category))
-        if @article.persisted?
-            redirect_to article_path(@article)
-        else
-            redirect_to new_article_path, notice: 'Every field needs to be filled in!'
-        end
-    end
-
-    private
-    def article_params
-        params.require(:article).permit(:title, :description, :content, :journalist)
-    end
-
-    def find_category
-        Category.find(params[:article][:category_id])
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root controller: :articles, action: :index
-  resources :articles, only: [:index, :show, :new, :create]
+  resources :articles, only: [:index, :show]
+  namespace :journalist do
+    resources :articles, only: [:new, :create]
+  end
+
   devise_for :users  
   resources :categories, only: [:show]
 end
