44194214473ddcc115336056673badcce179774d
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-14 11:36:08 +0100

    Adds unit tests for user role data columns and factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,22 @@ RSpec.describe User, type: :model do
   describe 'DB table' do
     it { is_expected.to have_db_column :email }
     it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :role }
+  end
+
+  describe 'roles' do 
+    it { is_expected.to respond_to :member! }
+    it { is_expected.to respond_to :member? }
+    it { is_expected.to respond_to :journalist! }
+    it { is_expected.to respond_to :journalist? }
   end
 
   describe 'FactoryBot' do
-    it 'should have valid Factory' do
-      expect(create(:user)).to be_valid
+    it 'for member is valid' do
+      expect(create(:member)).to be_valid
+    end
+    it 'for journalist is valid' do
+      expect(create(:journalist)).to be_valid
     end
   end
 end
