a0e8281b6746237697e636dc29ee52d19a6e3c0f
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-15 21:16:02 +0100

    Refactors if statement in article show page, using except instead of only in routes, refactors code in editor can delete articles feature file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root controller: :articles, action: :index
   resources :articles, only: [:index, :show]
   namespace :content_management do
-    resources :articles, only: [:new, :create, :edit, :update, :destroy]
+    resources :articles, except: [:show]
   end
 
   devise_for :users  
