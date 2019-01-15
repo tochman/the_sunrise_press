efcc263efd0c6436c5f729a1997936a6fb95dac8
Thomas <thomas@craftacademy.se>
2019-01-15 16:06:47 +0100

    sets up article model to accept attachment

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
@@ -10,8 +12,8 @@ RSpec.describe Article, type: :model do
 
   describe 'Validations' do
     it { is_expected.to validate_presence_of :title }
-    it { is_expected.to validate_presence_of :description } 
-    it { is_expected.to validate_presence_of :content } 
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :content }
   end
 
   describe 'Associations' do
@@ -25,4 +27,14 @@ RSpec.describe Article, type: :model do
     end
   end
 
+  describe 'Image attachment' do
+    let(:image) { File.open(fixture_path + '/basic_image.png') }
+
+    it 'can be attached to article' do
+      subject.image.attach(io: image,
+                           filename: 'attachment_1.png',
+                           content_type: 'image/png')
+      expect(subject.image).to be_attached
+    end
+  end
 end
