1e4c5ed980b04711a8c3a8832d0251b8c6b823a2
johannaanden <johannaanden@hotmail.com>
2019-01-17 13:10:59 +0100

    Creates Comment model and adds routes to it

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ApplicationRecord
     validates_presence_of :title, :description, :content
     belongs_to :category
     belongs_to :user
-
-
+    has_many :comments
+    
     def published?
         self.published
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_072156) do
+ActiveRecord::Schema.define(version: 2019_01_17_120511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2019_01_17_072156) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -52,4 +61,5 @@ ActiveRecord::Schema.define(version: 2019_01_17_072156) do
 
   add_foreign_key "articles", "categories"
   add_foreign_key "articles", "users"
+  add_foreign_key "comments", "articles"
 end
