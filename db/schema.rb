8c2f4df0d1694982ea0c291912bcc088992c86d3
johannaanden <johannaanden@hotmail.com>
2019-01-17 12:50:20 +0100

    Uppdates schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_100028) do
+ActiveRecord::Schema.define(version: 2019_01_17_072156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_01_15_100028) do
     t.bigint "category_id"
     t.bigint "user_id"
     t.boolean "published", default: false
+    t.boolean "premium", default: false
     t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
