cc29115fb162603fa2721fa5bede7ff2de5810b1
camron <camronmoradi@gmail.com>
2019-01-20 11:42:53 +0100

    Refactoring session with Thomas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    def user_cannot_access_content
+        !user_signed_in? || current_user.member?
+    end
+
+    def current_user_is_the_author
+        user_signed_in? && current_user == @article.user
+    end
+
+    def current_user_is_a_subscriber
+        user_signed_in? && current_user.subscriber?
+    end
 end
