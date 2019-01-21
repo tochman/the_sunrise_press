7f3e6c6fde5c102a205c771d4ebc817cc4561e15
johannaanden <johannaanden@hotmail.com>
2019-01-21 11:00:16 +0100

    Changes name on category_news to external_news

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
     def index
         @articles = Article.all
-        @category_news = NewsFeedService.get_feed('all', 'cnn', 'http://us.cnn.com', 4) 
+        @external_articles = NewsFeedService.get_feed('all', 'cnn', 'http://us.cnn.com', 4) 
     end
     
     def show
